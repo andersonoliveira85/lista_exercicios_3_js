751391ee19c2c830cf68681ff4b30645789ff348
Anderson Oliveira <51684215+andersonoliveira85@users.noreply.github.com>
2019-12-11 21:26:01 -0300

    Resposta da questão 01

---
+++ Ex1/js/script.js
@@ -0,0 +1,15 @@
+let i = 0;
+let soma= 0;
+let nota = 0;
+do{
+	nota = prompt ('Digite uma nota. Para sair digite: fim','');
+	console.log(nota);
+	if (nota !='fim'){
+	soma =+ nota;
+	i++;
+	}
+}while (nota!='fim');
+
+let mensagem = soma/i;
+
+alert (mensagem);
